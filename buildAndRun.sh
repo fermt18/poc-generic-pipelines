f6b0b9133c68bbd5a5487022fc645a2e74c2127e
fermt18 <fermt18@gmail.com>
2019-06-30 18:40:35 +0200

    Update buildAndRun.sh

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -3,8 +3,8 @@
 docker build -t pipeline -f Dockerfile.java .
 docker run \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  --env GITHUB_URL=https://github.com/fermt18/sample-java.git \
-  --env DOCKER_USERNAME=fermt18 \
-  --env DOCKER_PASSWORD=Fer15034 \
+  --env GITHUB_URL=<repository url> \
+  --env DOCKER_USERNAME=<user> \
+  --env DOCKER_PASSWORD=<password> \
   pipeline \
   /stages/00_run.sh
